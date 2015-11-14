7dc13702d6430e144576ea35dec188461b22a3e2
Ryan Staatz <rcstaatz@gmail.com>
2015-11-14 03:55:09 -0800

    Add MqttConnectionManager class - Add corresponding MqttConnectionManagerCallback - Adapt ConnectivityTestActivity code to use new MqttConnectionManager

--- app/src/main/java/com/rabidllamastudios/avigate/ConnectivityTestActivity.java
+++ app/src/main/java/com/rabidllamastudios/avigate/ConnectivityTestActivity.java
@@ -7,32 +7,18 @@ import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.eclipse.paho.android.service.*;
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-
 import java.util.ArrayList;
-import java.util.UUID;
 
 public class ConnectivityTestActivity extends AppCompatActivity {
 
-    public MqttAndroidClient mqttClient = null;
-    private TextView messageOutput;
-    private ArrayList<String> subscribedTopics;
-    private MqttCallback callback;
-
+    private MqttConnectionManager mMqttConnectionManager;
+    private TextView mMessageOutput;
+    private ArrayList<String> mSubscribedTopics;
     private static final String DEFAULT_SERVER = "test.mosquitto.org";
 
     @Override
@@ -42,38 +28,17 @@ public class ConnectivityTestActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        initializeLayout();
-    }
-
-    public void initializeCallback (){
-        callback = new MqttCallback() {
-            @Override
-            public void connectionLost(Throwable cause) {
-                messageOutput.append("\n" + "Connection lost: " + cause.getMessage());
-                setLayoutBasedOnConnectionStatus(false);
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) {
-                messageOutput.append("\n" + "Received: " + topic + "/" + new String (message.getPayload()));
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-            }
-        };
-    }
-
-    public void initializeLayout() {
-        subscribedTopics = new ArrayList();
+        setLayoutBasedOnConnectionStatus(false);
+        mSubscribedTopics = new ArrayList<>();
 
         EditText serverField = (EditText) findViewById(R.id.et_connect_hint_server);
         serverField.setText(DEFAULT_SERVER);
-        
-        messageOutput = (TextView) findViewById(R.id.tv_connect_value_messages);
-        messageOutput.setMovementMethod(new ScrollingMovementMethod());
+
+        mMessageOutput = (TextView) findViewById(R.id.tv_connect_value_messages);
+        mMessageOutput.setMovementMethod(new ScrollingMovementMethod());
 
         final Button subscribeButton = (Button) findViewById(R.id.button_subscribe);
         EditText topicFieldSubscribe = (EditText) findViewById(R.id.et_connect_hint_topic_subscribe);
@@ -85,9 +50,9 @@ public class ConnectivityTestActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (subscribedTopics.contains(s.toString())) {
+                if (mSubscribedTopics.contains(s.toString())) {
                     subscribeButton.setText(getString(R.string.button_unsubscribe));
-                } else if (subscribeButton.getText().toString() == getString(R.string.button_unsubscribe)) {
+                } else if (subscribeButton.getText().toString().equals(getString(R.string.button_unsubscribe))) {
                     subscribeButton.setText(getString(R.string.button_subscribe));
                 }
             }
@@ -98,17 +63,50 @@ public class ConnectivityTestActivity extends AppCompatActivity {
         });
     }
 
+    public MqttConnectionManagerCallback createMqttConnectionManagerCallback(){
+        return new MqttConnectionManagerCallback() {
+            @Override
+            public void connectionLost() {
+                mMessageOutput.append("\n" + "Connection lost");
+                mSubscribedTopics.clear();
+                setLayoutBasedOnConnectionStatus(false);
+            }
+
+            @Override
+            public void onConnect() {
+                mMessageOutput.append("\n" + "Client connected");
+                setLayoutBasedOnConnectionStatus(true);
+            }
+
+            @Override
+            public void messageArrived(String topic, String message) {
+                mMessageOutput.append("\n" + "Received: " + topic + "/" + message);
+            }
+        };
+    }
+
     public boolean hasWritePermissions() {
         PermissionsCheck permCheck = new PermissionsCheck();
         return permCheck.hasPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, permCheck.PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
     }
 
     public void connectButtonPressed(View view) {
-        Button connectionButton = (Button) findViewById(R.id.button_connect);
-        if (connectionButton.getText().equals(getString(R.string.button_connect))) {
-            connect();
-        } else if (connectionButton.getText().equals(getString(R.string.button_disconnect))) {
-            disconnect();
+        if (hasWritePermissions()) {
+            Button connectionButton = (Button) findViewById(R.id.button_connect);
+            if (connectionButton.getText().equals(getString(R.string.button_connect))) {
+                if (mMqttConnectionManager == null) {
+                    final EditText serverAddressField = (EditText) findViewById(R.id.et_connect_hint_server);
+                    mMqttConnectionManager = new MqttConnectionManager(this, createMqttConnectionManagerCallback(), serverAddressField.getText().toString(), 1883);
+                }
+                mMqttConnectionManager.start();
+            } else if (connectionButton.getText().equals(getString(R.string.button_disconnect))) {
+                mMqttConnectionManager.stop();
+                mMqttConnectionManager = null;
+                mMessageOutput.append("\n" + "Client disconnected");
+                setLayoutBasedOnConnectionStatus(false);
+            }
+        } else {
+            mMessageOutput.append("Grant access to storage permissions to connect");
         }
     }
 
@@ -118,22 +116,8 @@ public class ConnectivityTestActivity extends AppCompatActivity {
         String topicText = topicFieldPublish.getText().toString();
         String messageText = messageField.getText().toString();
 
-        MqttMessage message = new MqttMessage(messageText.getBytes());
-        message.setQos(2);
-        message.setRetained(false);
-
-        try {
-            mqttClient.publish(topicText, message);
-            messageField.setText("");
-            messageOutput.append("\n" + "Published: " + topicText + "/" + messageText);
-        } catch (MqttPersistenceException e) {
-            messageOutput.append("\n" + "Failed to publish:: " + topicText + "/" + messageText + ", " + e.getMessage());
-            e.printStackTrace();
-        } catch (MqttException e) {
-            messageOutput.append("\n" + "Failed to publish:: " + topicText + "/" + messageText + ", " + e.getMessage());
-            e.printStackTrace();
-        }
-
+        mMqttConnectionManager.publish(topicText, messageText);
+        mMessageOutput.append("\n" + "Published: " + topicText + "/" + messageText);
     }
 
     public void subscribeButtonPressed(View view) {
@@ -143,94 +127,21 @@ public class ConnectivityTestActivity extends AppCompatActivity {
         String topicText = topicFieldSubscribe.getText().toString();
 
         if (subscribeButton.getText().toString().equals(getString(R.string.button_unsubscribe))) {
-            try {
-                mqttClient.unsubscribe(topicText);
-                subscribedTopics.remove(topicText);
-                messageOutput.append("\n" + "Unsubscribed from: " + topicText);
-                topicFieldSubscribe.setText("");
-                if (topicFieldPublish.getText().toString().equals(topicText)) {
-                    topicFieldPublish.setText("");
-                }
-
-            } catch (MqttPersistenceException e) {
-                messageOutput.append("\n" + "Failed to unsubscribe from" + topicText + ", " + e.getMessage());
-                e.printStackTrace();
-
-            } catch (MqttException e) {
-                messageOutput.append("\n" + "Failed to unsubscribe from" + topicText + ", " + e.getMessage());
-                e.printStackTrace();
+            mMqttConnectionManager.unsubscribe(topicText);
+            mSubscribedTopics.remove(topicText);
+            mMessageOutput.append("\n" + "Unsubscribed from: " + topicText);
+            topicFieldSubscribe.setText("");
+            if (topicFieldPublish.getText().toString().equals(topicText)) {
+                topicFieldPublish.setText("");
             }
 
         } else {
-            try {
-                mqttClient.subscribe(topicFieldSubscribe.getText().toString(), 2);
-                subscribedTopics.add(topicText);
-                messageOutput.append("\n" + "Subscribed to: " + topicText);
-                topicFieldSubscribe.setText("");
-                topicFieldPublish.setText(topicText);
-            } catch (MqttPersistenceException e) {
-                messageOutput.append("\n" + "Failed to subscribe to: " + topicText + ", " + e.getMessage());
-                e.printStackTrace();
-            } catch (MqttException e) {
-                messageOutput.append("\n" + "Failed to subscribe to: " + topicText + ", " + e.getMessage());
-                e.printStackTrace();
-            }
+            mMqttConnectionManager.subscribe(topicText);
+            mSubscribedTopics.add(topicText);
+            mMessageOutput.append("\n" + "Subscribed to: " + topicText);
+            topicFieldSubscribe.setText("");
+            topicFieldPublish.setText(topicText);
         }
-    }
-
-    public void connect() {
-        if (hasWritePermissions()) {
-            MemoryPersistence memPersist = new MemoryPersistence();
-            String clientID = UUID.randomUUID().toString();
-            final EditText serverAddressField = (EditText) findViewById(R.id.et_connect_hint_server);
-            String serverAddress = serverAddressField.getText().toString();
-            serverAddress = "tcp://" + serverAddress + ":1883";
-            Log.i("URL", serverAddress);
-            mqttClient = new MqttAndroidClient(this, serverAddress, clientID, memPersist);
-            try {
-                mqttClient.connect(null, new IMqttActionListener() {
-
-                    @Override
-                    public void onSuccess(IMqttToken iMqttToken) {
-                        initializeCallback();
-                        mqttClient.setCallback(callback);
-                        if (!messageOutput.getText().toString().equals("")) {
-                            messageOutput.append("\n");
-                        }
-                        messageOutput.append("Client connected");
-                        setLayoutBasedOnConnectionStatus(true);
-                    }
-
-                    @Override
-                    public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
-                        if (!messageOutput.getText().toString().equals("")) {
-                            messageOutput.append("\n");
-                        }
-                        messageOutput.append("Client connection failed: " + throwable.getMessage());
-                        throwable.printStackTrace();
-                    }
-                });
-
-            } catch (MqttException e) {
-                e.printStackTrace();
-            }
-        } else {
-            if(!messageOutput.getText().toString().equals("")) {
-                messageOutput.append("\n");
-            }
-            messageOutput.append("Grant access to storage permissions to connect");
-        }
-    }
-
-    public void disconnect() {
-        try {
-            mqttClient.disconnect();
-            messageOutput.append("\n" + "Client disconnected");
-        } catch (MqttException e){
-            messageOutput.append("\n" + "Client disconnected with error: " + e.getMessage());
-            e.printStackTrace();
-        }
-        setLayoutBasedOnConnectionStatus(false);
     }
 
     private void setLayoutBasedOnConnectionStatus(boolean justConnected) {
@@ -295,29 +206,39 @@ public class ConnectivityTestActivity extends AppCompatActivity {
         }
     }
 
+    //TODO correctly implement below methods or remove class entirely
+
     @Override
-    public void onPause() {
-        super.onPause();
-        if(mqttClient != null) {
-            mqttClient.unregisterResources();
-            mqttClient.close();
-        }
+    public void onResume() {
+        super.onResume();
+        setLayoutBasedOnConnectionStatus(false);
+        mMessageOutput.append("\n" + "Client disconnected");
     }
 
-    public void onStop() {
-        super.onStop();
-        if(mqttClient != null) {
-            mqttClient.unregisterResources();
-            mqttClient.close();
+    @Override
+    public void onPause() {
+        if (mMqttConnectionManager !=null) {
+            mMqttConnectionManager.stop();
+            mMqttConnectionManager = null;
         }
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        if (mMqttConnectionManager !=null) {
+            mMqttConnectionManager.stop();
+            mMqttConnectionManager = null;
+        }
+        super.onStop();
     }
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
-        if (mqttClient != null) {
-            mqttClient.unregisterResources();
-            mqttClient.close();
+        if (mMqttConnectionManager !=null) {
+        mMqttConnectionManager.stop();
+            mMqttConnectionManager = null;
         }
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/rabidllamastudios/avigate/MqttConnectionManager.java
@@ -0,0 +1,148 @@
+package com.rabidllamastudios.avigate;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.util.UUID;
+
+/**
+ * TODO: write javadoc for class
+ * Created by Ryan on 11/11/15.
+ */
+public class MqttConnectionManager {
+
+    //Reconnect interval in ms
+    private static int RECONNECT_INTERVAL = 500;
+    private MqttAndroidClient mMqttAndroidClient;
+    private MqttConnectionManagerCallback mMqttConnectionManagerCallback;
+    private int mMqttQoS=0;
+    private Thread mReconnectThread;
+
+    public MqttConnectionManager(Context inputContext, MqttConnectionManagerCallback callback, String serverAddress, int portNumber) {
+        String serverURL= "tcp://" + serverAddress + ":" + Integer.toString(portNumber);
+        mMqttAndroidClient = new MqttAndroidClient(inputContext, serverURL, UUID.randomUUID().toString(), new MemoryPersistence());
+        mMqttConnectionManagerCallback = callback;
+    }
+
+    private void connect() {
+        try {
+            mMqttAndroidClient.connect(null, new IMqttActionListener() {
+
+                @Override
+                public void onSuccess(IMqttToken iMqttToken) {
+                    mMqttAndroidClient.setCallback(createMqttCallback());
+                    mMqttConnectionManagerCallback.onConnect();
+                    if (mReconnectThread != null) {
+                        mReconnectThread.interrupt();
+                        mReconnectThread = null;
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                    throwable.printStackTrace();
+                    if (mReconnectThread == null) {
+                        mReconnectThread = new Thread(new ReconnectRunnable());
+                        mReconnectThread.start();
+                    }
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publish(String topic, String message) {
+        MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+        mqttMessage.setQos(mMqttQoS);
+        try {
+            mMqttAndroidClient.publish(topic, mqttMessage);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        connect();
+    }
+
+    public void stop() {
+        if (mMqttAndroidClient != null) {
+            if (mMqttAndroidClient.isConnected())
+                unsubscribeAll();
+            }
+        mMqttAndroidClient.unregisterResources();
+        mMqttAndroidClient.close();
+
+        mMqttAndroidClient = null;
+    }
+
+    public void subscribe(String topic) {
+        try {
+            mMqttAndroidClient.subscribe(topic, mMqttQoS);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unsubscribe(String topic) {
+        try {
+            mMqttAndroidClient.unsubscribe(topic);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unsubscribeAll() {
+        this.unsubscribe("#");
+    }
+
+    private MqttCallback createMqttCallback() {
+        return new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable throwable) {
+                mMqttConnectionManagerCallback.connectionLost();
+                // Call connect so it will fail if there is no connection
+                connect();
+                if (throwable != null) {
+                    throwable.printStackTrace();
+                }
+            }
+
+            @Override
+            public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                mMqttConnectionManagerCallback.messageArrived(s, new String(mqttMessage.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        };
+    }
+
+    private class ReconnectRunnable implements Runnable {
+        @Override
+        public void run() {
+            while(mMqttAndroidClient !=null && !mMqttAndroidClient.isConnected()) {
+                connect();
+                try {
+                    Log.i("Avigate", "Reconnecting...");
+                    Thread.sleep(RECONNECT_INTERVAL);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        }
+    }
+}
