1e4a13e282928daf4c2db7936683022016fa389e
Ryan Staatz <rcstaatz@gmail.com>
2015-12-20 02:12:53 -0800

    Create CustomFragmentPagerAdapter and NonSwipeableViewPager - CustomFragmentPagerAdapter can create any number of Tabs / Fragments - Use NonSwipeableViewPager because of SeekBars in ServoOutputFragment

---
+++ app/src/main/java/com/rabidllamastudios/avigate/CustomFragmentPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.rabidllamastudios.avigate;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ryan on 12/19/15.
+ * This FragmentPagerAdapter class configures any number of Tab Title and Fragment pairs
+ * This class is intended for use with the TabLayout class
+ */
+public class CustomFragmentPagerAdapter extends FragmentPagerAdapter {
+    private List<String> mTabTitles;
+    private List<Fragment> mFragments;
+
+    public CustomFragmentPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+        mTabTitles = new ArrayList<>();
+        mFragments = new ArrayList<>();
+    }
+
+    //Adds a Tab Title and Fragment pair in a given position in the associated List instance var
+    public void addEntry(int position, String tabTitle, Fragment fragment) {
+        mTabTitles.add(position, tabTitle);
+        mFragments.add(position, fragment);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mTabTitles.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTabTitles.get(position);
+    }
+
+}
---
+++ app/src/main/java/com/rabidllamastudios/avigate/NonSwipeableViewPager.java
@@ -0,0 +1,35 @@
+package com.rabidllamastudios.avigate;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by Ryan on 12/19/15.
+ * Creates a ViewPager that disables the ability to use a horizontal swipe to navigate between tabs
+ * Taken from StackOverflow post: http://stackoverflow.com/questions/9650265/
+ */
+public class NonSwipeableViewPager extends ViewPager {
+
+    public NonSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+}
