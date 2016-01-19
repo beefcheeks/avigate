ef230bccc35f73fec371f238681dd186792260f8
Ryan Staatz <rcstaatz@gmail.com>
2016-01-18 22:32:32 -0800

    Update CraftActivity to use MasterFlightService - Remove all traces of other services now started in MasterFlightService - Remove onDestroy and add OnPause and OnResume methods - Make small changes to local variable names and formatting

--- app/src/main/java/com/rabidllamastudios/avigate/activities/CraftActivity.java
+++ app/src/main/java/com/rabidllamastudios/avigate/activities/CraftActivity.java
@@ -1,6 +1,7 @@
 package com.rabidllamastudios.avigate.activities;
 
 import android.Manifest;
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +10,6 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.rabidllamastudios.avigate.R;
@@ -17,26 +17,21 @@ import com.rabidllamastudios.avigate.helpers.PermissionsChecker;
 import com.rabidllamastudios.avigate.models.ConnectionPacket;
 import com.rabidllamastudios.avigate.models.CraftStatePacket;
 import com.rabidllamastudios.avigate.models.ArduinoPacket;
+import com.rabidllamastudios.avigate.services.MasterFlightService;
 import com.rabidllamastudios.avigate.services.NetworkService;
-import com.rabidllamastudios.avigate.services.FlightControlService;
-import com.rabidllamastudios.avigate.services.SensorService;
 import com.rabidllamastudios.avigate.services.UsbSerialService;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class CraftActivity extends AppCompatActivity {
 
-    private static final String CLASS_NAME = CraftActivity.class.getSimpleName();
     private static final String DEGREES = " °";
     private static final String DEGREES_PER_SECOND = " °/s";
-    //Sensor broadcast rate in milliseconds (ms)
-    private static final int SENSOR_BROADCAST_RATE = 100;
 
-    private Intent mNetworkService = null;
-    private Intent mFlightControlService = null;
-    private Intent mSensorService = null;
-    private Intent mUsbSerialService = null;
+    private IntentFilter mArduinoOutputIntentFilter;
+    private IntentFilter mConnectionIntentFilter;
+    private IntentFilter mCraftStateIntentFilter;
+    private IntentFilter mUsbIntentFilter;
+    private PermissionsChecker mPermissionsChecker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,91 +43,83 @@ public class CraftActivity extends AppCompatActivity {
         assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //Register broadcast receiver for any ConnectionPacket Intents
-        IntentFilter connectionIntentFilter = new IntentFilter(ConnectionPacket.INTENT_ACTION);
-        registerReceiver(mConnectionReceiver, connectionIntentFilter);
+        //Initialize IntentFilters for registering BroadcastReceivers in onResume() method
+        mArduinoOutputIntentFilter = new IntentFilter(ArduinoPacket.INTENT_ACTION_OUTPUT);
+        mConnectionIntentFilter = new IntentFilter(ConnectionPacket.INTENT_ACTION);
+        mCraftStateIntentFilter = new IntentFilter(CraftStatePacket.INTENT_ACTION);
 
-        //Register broadcast receiver for any CraftStatePacket (sensor-related) Intents
-        registerReceiver(mCraftStateReceiver, new IntentFilter(CraftStatePacket.INTENT_ACTION));
+        //Initialize and add multiple Intent actions for mUsbIntentFilter
+        mUsbIntentFilter = new IntentFilter();
+        mUsbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_READY);
+        mUsbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_PERMISSION_GRANTED);
+        mUsbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_NO_USB);
+        mUsbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_DISCONNECTED);
+        mUsbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_NOT_SUPPORTED);
+        mUsbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_PERMISSION_NOT_GRANTED);
 
-        //Register broadcast receiver for USB-related Intents
-        IntentFilter usbIntentFilter = new IntentFilter();
-        usbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_READY);
-        usbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_PERMISSION_GRANTED);
-        usbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_NO_USB);
-        usbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_DISCONNECTED);
-        usbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_NOT_SUPPORTED);
-        usbIntentFilter.addAction(UsbSerialService.INTENT_ACTION_USB_PERMISSION_NOT_GRANTED);
-        registerReceiver(mUsbReceiver, usbIntentFilter);
-
-        //Initialize mServoOutputFilter IntentFilter and register associated BroadcastReceiver
-        IntentFilter arduinoOutputIntentFilter =
-                new IntentFilter(ArduinoPacket.INTENT_ACTION_OUTPUT);
-        registerReceiver(mArduinoOutputReceiver, arduinoOutputIntentFilter);
-
-        //Configure and start the UsbSerialService
-        mUsbSerialService = UsbSerialService.getConfiguredIntent(this);
-        startService(mUsbSerialService);
-
-        //Configure the SensorService Intent
-        mSensorService = SensorService.getConfiguredIntent(this, SENSOR_BROADCAST_RATE);
-
-        //Configure the start service Intent
-        IntentFilter startServiceIntentFilter = new IntentFilter(
-                FlightControlService.INTENT_ACTION_CONFIGURE_FLIGHT_CONTROL_SERVICE);
-        registerReceiver(mStartServiceReceiver, startServiceIntentFilter);
-
-        //Check for location permissions before starting the sensor service
-        PermissionsChecker permissionsChecker =
-                new PermissionsChecker(this, mPermissionsCheckerCallback);
-        if (permissionsChecker.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION,
-                PermissionsChecker.PERMISSIONS_REQUEST_READ_LOCATION_FINE)) {
-            startService(mSensorService);
-        }
-
-        //Configure and start NetworkService.
-        List<String> localSubs = new ArrayList<>();
-        List<String> remoteSubs = new ArrayList<>();
-        localSubs.add(CraftStatePacket.INTENT_ACTION);
-        localSubs.add(ArduinoPacket.INTENT_ACTION_OUTPUT);
-        localSubs.add(UsbSerialService.INTENT_ACTION_USB_READY);
-        localSubs.add(UsbSerialService.INTENT_ACTION_USB_PERMISSION_GRANTED);
-        localSubs.add(UsbSerialService.INTENT_ACTION_NO_USB);
-        localSubs.add(UsbSerialService.INTENT_ACTION_USB_DISCONNECTED);
-        localSubs.add(UsbSerialService.INTENT_ACTION_USB_NOT_SUPPORTED);
-        localSubs.add(UsbSerialService.INTENT_ACTION_USB_PERMISSION_NOT_GRANTED);
-        remoteSubs.add(FlightControlService.INTENT_ACTION_CONFIGURE_FLIGHT_CONTROL_SERVICE);
-        remoteSubs.add(ArduinoPacket.INTENT_ACTION_INPUT);
-        mNetworkService = NetworkService.getConfiguredIntent(this, localSubs, remoteSubs,
-                NetworkService.DeviceType.CRAFT);
-        startService(mNetworkService);
+        //Initialize mPermissionsChecker
+        mPermissionsChecker = new PermissionsChecker(this, mPermissionsCheckerCallback);
     }
 
-    // If the user allows location permissions, start SensorService
-    private PermissionsChecker.Callback mPermissionsCheckerCallback = new PermissionsChecker.Callback() {
-        @Override
-        public void permissionGranted(int permissionsConstant) {
-            if (permissionsConstant == PermissionsChecker.PERMISSIONS_REQUEST_READ_LOCATION_FINE) {
-                startService(mSensorService);
-            }
-        }
-    };
-
     @Override
-    public void onDestroy() {
+    public void onPause() {
         //Unregister all receivers
         unregisterReceiver(mArduinoOutputReceiver);
         unregisterReceiver(mConnectionReceiver);
         unregisterReceiver(mCraftStateReceiver);
-        unregisterReceiver(mStartServiceReceiver);
         unregisterReceiver(mUsbReceiver);
-        //Stop all services (if running)
-        if (mNetworkService != null) stopService(mNetworkService);
-        if (mFlightControlService != null) stopService(mFlightControlService);
-        if (mSensorService != null) stopService(mSensorService);
-        if (mUsbSerialService != null) stopService(mUsbSerialService);
         //Call super method
-        super.onDestroy();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        //Register all receiver methods
+        registerReceiver(mArduinoOutputReceiver, mArduinoOutputIntentFilter);
+        registerReceiver(mConnectionReceiver, mConnectionIntentFilter);
+        registerReceiver(mCraftStateReceiver, mCraftStateIntentFilter);
+        registerReceiver(mUsbReceiver, mUsbIntentFilter);
+        //Check for location permissions before attempting to start the MasterFlightService
+        if (mPermissionsChecker.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION,
+                PermissionsChecker.PERMISSIONS_REQUEST_READ_LOCATION_FINE)) {
+            startMasterFlightService();
+        }
+        //Request the connection status from NetworkService
+        sendBroadcast(new Intent(NetworkService.INTENT_ACTION_REQUEST_CONNECTION_STATUS));
+        //Call super method
+        super.onResume();
+    }
+
+    // If the user allows location permissions, start the MasterFlightService
+    private PermissionsChecker.Callback mPermissionsCheckerCallback =
+            new PermissionsChecker.Callback() {
+                @Override
+                public void permissionGranted(int permissionsConstant) {
+                    if (permissionsConstant ==
+                            PermissionsChecker.PERMISSIONS_REQUEST_READ_LOCATION_FINE) {
+                        startMasterFlightService();
+                    }
+                }
+            };
+
+    //Starts the MasterFlightService if it is not already running
+    private void startMasterFlightService() {
+        if (!isServiceRunning(MasterFlightService.class)) {
+            startService(MasterFlightService.getConfiguredIntent(getApplicationContext()));
+        }
+    }
+
+    //Checks if a given service class is running
+    //Taken from: http://stackoverflow.com/a/5921190
+    private boolean isServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service :
+                manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     //Listens for responses from the connected USB serial device and updates the output TextView
@@ -171,21 +158,26 @@ public class CraftActivity extends AppCompatActivity {
         }
     };
 
+    //Listens for ConnectionPacket Intents and updates various TextViews accordingly
     private BroadcastReceiver mConnectionReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             //If the Intent is type ConnectionPacket, update corresponding TextView value
             if (intent.getAction().equals(ConnectionPacket.INTENT_ACTION)) {
                 TextView connectionStatusTV = (TextView) findViewById(R.id.tv_craft_value_connect);
-                ConnectionPacket connectionPacket = new ConnectionPacket(intent.getExtras());
-                if (connectionPacket.isConnected()) {
-                    connectionStatusTV.setText(getResources().getString(R.string.tv_placeholder_connected));
-                    //Request the status of the Arduino after a connection is established
+                //If ConnectionPacket method isConnected is true, show the network is connected
+                if (new ConnectionPacket(intent.getExtras()).isConnected()) {
+                    connectionStatusTV.setText(
+                            getResources().getString(R.string.tv_placeholder_connected));
+                    //TODO get Arduino status without notifying NetworkService of status response
+                    //Request the status of the Arduino after reconnection
                     ArduinoPacket arduinoPacket = new ArduinoPacket();
                     arduinoPacket.addStatusRequest();
                     sendBroadcast(arduinoPacket.toIntent(ArduinoPacket.INTENT_ACTION_INPUT));
+                //If ConnectionPacket method isConnected is false, show the network is disconnected
                 } else {
-                    connectionStatusTV.setText(getResources().getString(R.string.tv_placeholder_disconnected));
+                    connectionStatusTV.setText(
+                            getResources().getString(R.string.tv_placeholder_disconnected));
                 }
             }
         }
@@ -250,23 +242,9 @@ public class CraftActivity extends AppCompatActivity {
                 //Process barometric pressure data and update corresponding TextViews
                 CraftStatePacket.BarometricPressure barometricPressure =
                         craftStatePacket.getBarometricPressure();
-                String altitude = String.valueOf(barometricPressure.getPressure()) + " hPa";
+                String pressure = String.valueOf(barometricPressure.getPressure()) + " hPa";
                 TextView pressureTV = (TextView) findViewById(R.id.tv_craft_value_barometer);
-                pressureTV.setText(altitude);
-            }
-        }
-    };
-
-    //Listens for any Intents that are used to start other services
-    private BroadcastReceiver mStartServiceReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(
-                    FlightControlService.INTENT_ACTION_CONFIGURE_FLIGHT_CONTROL_SERVICE)) {
-                Log.i(CLASS_NAME, "FlightControlService start command received");
-                intent.setClass(getApplicationContext(), FlightControlService.class);
-                mFlightControlService = intent;
-                startService(mFlightControlService);
+                pressureTV.setText(pressure);
             }
         }
     };
